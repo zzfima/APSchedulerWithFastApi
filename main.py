cfa7f3a1113eedf64ab1216bab6fcf6579bcc1e6
zfima <zzfima@gmail.com>
2020-12-03 10:32:16 +0200

    all files

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import asyncio
 import threading
-import time
 
 import uvicorn
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import FastAPI
 from starlette import status
@@ -12,7 +9,6 @@ from starlette.responses import Response
 threads_names = set()
 app = FastAPI()
 scheduler = BackgroundScheduler()
-# scheduler = AsyncIOScheduler()
 
 print(scheduler.state)
 
@@ -28,11 +24,11 @@ async def start_scheduler():
 @app.get("/add_job")
 async def add_job():
     print('Add job to scheduler')
-    scheduler.add_job(job_function, 'interval', seconds=1)
+    scheduler.add_job(do_job, 'interval', seconds=5)
     return Response(status_code=status.HTTP_200_OK)
 
 
-def job_function():
+def do_job():
     ti = threading.get_ident()
     if ti not in threads_names:
         threads_names.add(ti)
@@ -40,10 +36,3 @@ def job_function():
 
 
 uvicorn.run(app)
-
-# # Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
-# try:
-#     asyncio.get_event_loop().run_forever()
-# except (KeyboardInterrupt, SystemExit):
-#     pass
-
